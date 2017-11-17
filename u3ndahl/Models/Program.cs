471f335b5211ca99de031b847110ee6e18f49304
nada1600 <nada1600@student.miun.se>
2017-11-17 20:11:54 +0100

    Lagt till klass + properties i Models

---
+++ u3ndahl/Models/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u3ndahl.Models
+{
+    public class Program
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public int time { get; set; }
+        public string category { get; set; }
+        public int channel { get; set; }
+
+    }
+}
