c5680937758d0895de4f45cfb907fbc2b0f054ff
nada1600 <nada1600@student.miun.se>
2017-11-21 12:07:54 +0100

    Lagt till ett programobjekt och skapat en lista för kanal 1.

--- u3ndahl/Data/Data.cs
+++ u3ndahl/Data/Data.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using u3ndahl.Models;
 
 namespace u3ndahl.Data
 {
     public class Data
     {
+        Program Program1 = new Program()
+        {
+            title = "Alfons Elefanter",
+            category = "Barn",
+            time = 18
+        };
+
+        List<Program> kanal1 = new List<Program>();
     }
 }
